87db182c4be8ba551f792adbe2e9213488067f86
Gabriel Kocenko <gabryelkocenko@gmail.com>
2023-08-17 11:00:20 +0200

    Setting up transformer class

---
+++ transformer.py
@@ -0,0 +1,40 @@
+import torch.nn as nn
+
+
+class EncoderBlock(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        # Managing image input
+
+        # Self attention
+
+
+class SingleHeadAttention(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+
+class TransformerBlock(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        # Self attention
+
+        # Cross attention
+
+        # Feed forward
+
+
+class TransformerDecoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        # Embeddings (with positional)
+
+
