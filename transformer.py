41ace199910664ac70e47ce4ee1aa371879983cf
Gabriel Kocenko <gabryelkocenko@gmail.com>
2023-08-17 11:23:22 +0200

    Defined encoder block

--- transformer.py
+++ transformer.py
@@ -4,10 +4,11 @@ import torch.nn as nn
 class EncoderBlock(nn.Module):
     def __init__(self):
         super().__init__()
+        self.flattener = nn.Flatten(start_dim=2, end_dim=3)
 
-        # Managing image input
-
-        # Self attention
+    def forward(self, x):
+        # Expected input (B, C, H, W)
+        return self.flattener(x)
 
 
 class SingleHeadAttention(nn.Module):
