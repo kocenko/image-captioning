ae3d2e8d8ce615ca30870fab22e5a652c8787fe7
Gabriel Kocenko <gabryelkocenko@gmail.com>
2023-08-18 15:16:54 +0200

    Listed what to do for training loop

---
+++ train.py
@@ -0,0 +1,11 @@
+# TODO:
+# > Training loop
+# > Saving the model
+# > Searching and loading the newest model
+# > Keeping track of the hyperparameters along the training
+# > Ensuring the possibility to restart training in any moment (without reloading the dataset etc.)
+# > Saving loss function graphs
+# > Saving the captions for one chosen image along the training process
+# ...
+# > Plotting attention maps
+# > Check the dimensions of the transformer and write them down
