0c46396981fd70c6343165236d4ca3f98c28f0dd
Gabriel Kocenko <gabryelkocenko@gmail.com>
2023-08-23 16:58:37 +0200

    Adjusted to the changes of tokenizer

--- dataset.py
+++ dataset.py
@@ -1,8 +1,6 @@
 from tokenizer import Tokenizer
 from feature_extractor import FeatureExtractor
 
-import torch
-from torch import Tensor
 from torch.utils.data import Dataset, DataLoader
 
 
@@ -16,11 +14,11 @@ class ImageCaptionDataset(Dataset):
         return len(self.tokenizer.captions)
 
     def __getitem__(self, item: int):
-        caption = self.tokenizer.encode(f"{self.tokenizer.START_TOKEN} {self.tokenizer.captions[item]} {self.tokenizer.END_TOKEN}")
+        caption = self.tokenizer.encode(f"{self.tokenizer.start_token} {self.tokenizer.captions[item]} {self.tokenizer.end_token}")
         input_caption = caption[..., :-1]
         label_caption = caption[..., 1:]
         raw_image = self.extractor.get_image_from_file(self.tokenizer.image_paths[item])
-        transformed_image = self.extractor.feed(raw_image.unsqueeze(0).to(self.device)).squeeze(0)
+        transformed_image = self.extractor.feed(raw_image.unsqueeze(0).to(self.device)).squeeze(0)  # (C, H, W)
         return transformed_image, input_caption, label_caption
 
 
@@ -33,6 +31,6 @@ if __name__ == "__main__":
 
     fe = FeatureExtractor(device="cpu")
     fe.slice_net(97)
-    tk = Tokenizer(raw_file, folder, device="cpu")
+    tk = Tokenizer(raw_file, folder)
     ds = ImageCaptionDataset(tk, fe, device="cpu")
     dl = DataLoader(ds, batch_size=50, shuffle=True)
