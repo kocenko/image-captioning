213ccf5fd03461ef262bea4dcdbe4e08a36b3302
Gabriel Kocenko <gabryelkocenko@gmail.com>
2023-08-28 11:20:47 +0200

    Added docstrings

--- dataset.py
+++ dataset.py
@@ -6,19 +6,54 @@ from torch.utils.data import Dataset, DataLoader
 
 
 class ImageCaptionDataset(Dataset):
-    def __init__(self, tokenizer: Tokenizer, extractor: FeatureExtractor, device: str = "cuda"):
+    """
+    Custom class for dataset
+
+    Attributes:
+        tokenizer (Tokenizer): custom tokenizer
+        extractor (FeatureExtractor): convolutional neural network used for feature extraction
+        device (str): string indicating which device will be used for calculations
+    """
+
+    def __init__(self, tokenizer: Tokenizer, extractor: FeatureExtractor, device: str = "cuda") -> None:
+        """
+        Dataset initialization
+
+        Args:
+            tokenizer (Tokenizer): custom tokenizer
+            extractor (FeatureExtractor): convolutional neural network used for feature extraction
+            device (str): string indicating which device will be used for calculations
+        """
+
         self.tokenizer: Tokenizer = tokenizer
         self.extractor: FeatureExtractor = extractor
         self.device: str = device
 
     def __len__(self) -> int:
+        """
+        Returns:
+             Length of the dataset
+        """
+
         return len(self.tokenizer.captions)
 
     def __getitem__(self, item: int):
-        caption = self.tokenizer.encode(f"{self.tokenizer.start_token} {self.tokenizer.captions[item]} {self.tokenizer.end_token}")
+        """
+        Fetches a sample of the given index
+
+        Args:
+             item (int): index of the sample to return
+
+        Returns:
+            A tuple with three elements: tensor of the transformed image and tokenized input and output (label) captions
+        """
+
+        full_caption = f"{self.tokenizer.start_token} {self.tokenizer.captions[item]} {self.tokenizer.end_token}"
+        caption = self.tokenizer.encode(full_caption)
         caption = torch.tensor(caption, device=self.device)
         input_caption = caption[..., :-1]
         label_caption = caption[..., 1:]
+
         raw_image = self.extractor.get_image_from_file(self.tokenizer.image_paths[item])
         transformed_image = self.extractor.feed(raw_image.unsqueeze(0).to(self.device)).squeeze(0)  # (C, H, W)
         return transformed_image, input_caption, label_caption
@@ -32,7 +67,6 @@ if __name__ == "__main__":
         raw_file = f.read()
 
     fe = FeatureExtractor(device="cpu")
-    fe.slice_net(97)
     tk = Tokenizer(raw_file, folder)
     ds = ImageCaptionDataset(tk, fe, device="cpu")
     dl = DataLoader(ds, batch_size=50, shuffle=True)
