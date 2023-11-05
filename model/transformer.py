4ae41cb0ca801b16794fac77fc28e7dd8643b9f6
Gabriel Kocenko <gabryelkocenko@gmail.com>
2023-11-05 02:08:53 +0100

    Clean-up after testing

--- model/transformer.py
+++ model/transformer.py
@@ -55,41 +55,41 @@ class MultiHeadAttention(nn.Module):
         key   = key.view(B, T_k, self.num_heads, self.key_dim).transpose(1, 2)    # [B, num_heads, T_k, key_dim]
         value = value.view(B, T_k, self.num_heads, self.key_dim).transpose(1, 2)  # [B, num_heads, T_v, val_dim]
 
-        np.save(f'../numpy_logs/torch_key_{type_of_att}.npy', key.transpose(1, 2).detach().cpu().numpy())
-        np.save(f'../numpy_logs/torch_value_{type_of_att}.npy', value.transpose(1, 2).detach().cpu().numpy())
+        # np.save(f'../numpy_logs/torch_key_{type_of_att}.npy', key.transpose(1, 2).detach().cpu().numpy())
+        # np.save(f'../numpy_logs/torch_value_{type_of_att}.npy', value.transpose(1, 2).detach().cpu().numpy())
 
 
         # Scaling query
         query /= math.sqrt(self.key_dim)
-        np.save(f'../numpy_logs/torch_query_{type_of_att}.npy', query.transpose(1, 2).detach().cpu().numpy())
+        # np.save(f'../numpy_logs/torch_query_{type_of_att}.npy', query.transpose(1, 2).detach().cpu().numpy())
 
 
         # Dot-product between the query and the key
         affinity = query @ key.transpose(-2, -1)  # [B, num_heads, T_q, T_k]
         
-        np.save(f'../numpy_logs/torch_affinity_{type_of_att}.npy', affinity.detach().cpu().numpy())
+        # np.save(f'../numpy_logs/torch_affinity_{type_of_att}.npy', affinity.detach().cpu().numpy())
 
         # Masked softmax
         if mask:
             causal_mask = torch.tril(torch.ones(T_q, T_k, device=self.device)).view(1, 1, T_q, T_k)
             affinity += (causal_mask == 0) * -1e9
-        np.save(f'../numpy_logs/torch_masked_{type_of_att}.npy', affinity.detach().cpu().numpy())
+        # np.save(f'../numpy_logs/torch_masked_{type_of_att}.npy', affinity.detach().cpu().numpy())
         affinity = self.softmax(affinity)
-        np.save(f'../numpy_logs/torch_softmax_{type_of_att}.npy', affinity.detach().cpu().numpy())
+        # np.save(f'../numpy_logs/torch_softmax_{type_of_att}.npy', affinity.detach().cpu().numpy())
         affinity = self.attention_dropout(affinity)
-        np.save(f'../numpy_logs/torch_dropout_{type_of_att}.npy', affinity.detach().cpu().numpy())
+        # np.save(f'../numpy_logs/torch_dropout_{type_of_att}.npy', affinity.detach().cpu().numpy())
 
 
         # # Output score
         # score = affinity @ value  # [B, num_heads, T_v, val_dim]
         # score = score.transpose(1, 2)
         score = torch.einsum('acbe,aecd->abcd', affinity, value.transpose(1, 2))
-        np.save(f'../numpy_logs/torch_out_{type_of_att}.npy', score.detach().cpu().numpy())
+        # np.save(f'../numpy_logs/torch_out_{type_of_att}.npy', score.detach().cpu().numpy())
 
         # Output projection
         score = score.reshape(B, T_q, -1)
         score = self.output_projection(score)
-        np.save(f'../numpy_logs/torch_projection_{type_of_att}.npy', score.detach().cpu().numpy())
+        # np.save(f'../numpy_logs/torch_projection_{type_of_att}.npy', score.detach().cpu().numpy())
 
         score = self.output_dropout(score)
         return score
@@ -124,26 +124,24 @@ class TransformerBlock(nn.Module):
     def forward(self, image, caption):
         # Note: pre-norm formulation can be used
         sa = self.self_attention(caption, caption, caption, True)
-        np.save('../numpy_logs/torch_self_att.npy', sa.detach().cpu().numpy())
+        # np.save('../numpy_logs/torch_self_att.npy', sa.detach().cpu().numpy())
         x = torch.add(caption, sa)
-        np.save('../numpy_logs/torch_self_add.npy', x.detach().cpu().numpy())
+        # np.save('../numpy_logs/torch_self_add.npy', x.detach().cpu().numpy())
         x = self.layer_normalization_1(x)
-        np.save('../numpy_logs/torch_self_norm.npy', x.detach().cpu().numpy())
+        # np.save('../numpy_logs/torch_self_norm.npy', x.detach().cpu().numpy())
 
-        mock_image = torch.ones((32, 49, 576)).to(torch.float32).to("cuda")
-        mock_caption = torch.ones((32, 20, 256)).to(torch.int32).to("cuda")
-        cr = self.cross_attention(mock_caption, mock_image, mock_image, False)
-        np.save('../numpy_logs/torch_cross_att.npy', cr.detach().cpu().numpy())
-        x = torch.add(mock_caption, cr)
-        np.save('../numpy_logs/torch_cross_add.npy', x.detach().cpu().numpy())
+        cr = self.cross_attention(x, image, image, False)
+        # np.save('../numpy_logs/torch_cross_att.npy', cr.detach().cpu().numpy())
+        x = torch.add(x, cr)
+        # np.save('../numpy_logs/torch_cross_add.npy', x.detach().cpu().numpy())
         x = self.layer_normalization_2(x)
-        np.save('../numpy_logs/torch_cross_norm.npy', x.detach().cpu().numpy())
+        # np.save('../numpy_logs/torch_cross_norm.npy', x.detach().cpu().numpy())
 
         x = x + self.feed_forward(x)
-        np.save('../numpy_logs/torch_ff_add.npy', x.detach().cpu().numpy())
+        # np.save('../numpy_logs/torch_ff_add.npy', x.detach().cpu().numpy())
 
         x = self.layer_normalization_3(x)
-        np.save('../numpy_logs/torch_ff_norm.npy', x.detach().cpu().numpy())
+        # np.save('../numpy_logs/torch_ff_norm.npy', x.detach().cpu().numpy())
 
         return x
 
@@ -230,16 +228,16 @@ class Decoder(nn.Module):
 
     def forward(self, image, caption):
         image = self.image_flattener(image)
-        np.save('../numpy_logs/torch_image.npy', image.detach().cpu().numpy())
+        # np.save('../numpy_logs/torch_image.npy', image.detach().cpu().numpy())
 
         x = self.embedding(caption)
-        np.save('../numpy_logs/torch_embedding.npy', x.detach().cpu().numpy())
+        # np.save('../numpy_logs/torch_embedding.npy', x.detach().cpu().numpy())
 
 
         for block in self.blocks:
             x = block(image, x)
 
         logits = self.output_layer(x)
-        np.save('../numpy_logs/torch_output_layer.npy', logits.detach().cpu().numpy())
+        # np.save('../numpy_logs/torch_output_layer.npy', logits.detach().cpu().numpy())
 
         return logits
