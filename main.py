Moved masking to transformer, small fixes

main.py
@@ -4,7 +4,7 @@ from model.transformer import Decoder
 from data_processing.tokenizer import Tokenizer
 from data_processing.feature_extractor import FeatureExtractor
 from data_processing.dataset import DataCachingManager, ImageCaptionDataset
-from data_processing.loader import load_flickr8k
+from data_processing.loader import load_flickr8k, load_flickr30k
 from model.train import Trainer
 
 import torch
@@ -14,27 +14,35 @@ from torch.utils.tensorboard import SummaryWriter
 
 
 def main():
-    tokens_path = "../dataset/Flickr8k.token.txt"
-    train_path = "../dataset/Flickr_8k.trainImages.txt"
-    valid_path = "../dataset/Flickr_8k.devImages.txt"
-    test_path = "../dataset/Flickr_8k.testImages.txt"
-    images_path = "../dataset/images"
+
     sample_image = "./evaluation/sample_images/surfing.jpg"
     checkpoints_folder = "./evaluation/checkpoints"
     summary_folder = "./evaluation/summary"
 
-    train_ds, valid_ds, test_ds = load_flickr8k(tokens_path, train_path, valid_path, test_path, images_path)
+    # # Option 1.
+    # tokens_path = "../dataset/Flickr8k.token.txt"
+    # train_path = "../dataset/Flickr_8k.trainImages.txt"
+    # valid_path = "../dataset/Flickr_8k.devImages.txt"
+    # test_path = "../dataset/Flickr_8k.testImages.txt"
+    # images_path = "../dataset/images"
+    # train_ds, valid_ds, test_ds = load_flickr8k(tokens_path, train_path, valid_path, test_path, images_path)
+
+    # Option 2.
+    tokens_path = "../dataset/flickr30k/captions.txt"
+    images_path = "../dataset/flickr30k/Images"
+    train_ds, valid_ds, test_ds = load_flickr30k(tokens_path, images_path)
+
 
     hyperparameters = {
         "batches": 32,
-        "context_length": 50,
+        "context_length": 60,
         "vocabulary_size": 5000,
         "banned_tokens": ["<unknown>", "<start>", ""],
         "embeddings_number": 256,
         "dropout_rate": 0.5,
         "learning_rate": 1e-4,
         "epochs": 100,
-        "blocks_number": 1,
+        "blocks_number": 2,
         "heads_number": 2,
         "head_size": 128,
         "net_slice_index": "features.12",
@@ -52,24 +60,28 @@ def main():
         fe.slice_net(hyperparameters["net_slice_index"], overwrite_model=True)
 
     tk = Tokenizer([caption for _, caption in train_ds], max_sequence_size=hyperparameters["context_length"], vocabulary_size=hyperparameters["vocabulary_size"])
-    train_set = ImageCaptionDataset(train_ds, tk, fe, hyperparameters["device"])
-    dl = DataLoader(train_set, batch_size=32, shuffle=True, collate_fn=train_set.collate)
 
-    # sh = DataCachingManager(tk, fe, batch_size=hyperparameters["batches"], shard_size=2000, device=hyperparameters["device"])
-    # # sh.save_shards(train_ds, "train", "shards/train")
-    # # sh.save_shards(valid_ds, "valid", "shards/valid")
-    # # sh.save_shards(test_ds, "test", "shards/test")
-    # sh.load_shards(["shards/train", "shards/valid", "shards/test"], ["train", "valid", "test"])
+    # Option 1.
+    sh = DataCachingManager(tk, fe, batch_size=hyperparameters["batches"], shard_size=2000, device=hyperparameters["device"])
+    # sh.save_shards(train_ds, "train", "shards/train")
+    # sh.save_shards(valid_ds, "valid", "shards/valid")
+    # sh.save_shards(test_ds, "test", "shards/test")
+    sh.load_shards(["shards/train", "shards/valid", "shards/test"], ["train", "valid", "test"])
 
-    # # Updating dependent hyperparameters
-    # hyperparameters["image_channels"] = fe.feed(fe.get_image_from_file(sample_image).unsqueeze(0)).shape[1]
-    # hyperparameters["word_count"] = tk.counter
-    # hyperparameters["encode_map"] = tk.encode_map
+    # # Option 2.
+    # train_set = ImageCaptionDataset(train_ds, tk, fe, hyperparameters["device"])
+    # valid_set = ImageCaptionDataset(valid_ds, tk, fe, hyperparameters["device"])
+    # sh = (train_set, valid_set)
 
-    # # Preparing folders for logging
-    # for path in [checkpoints_folder, summary_folder]:
-    #     if not os.path.exists(path):
-    #         os.makedirs(path)
+    # Updating dependent hyperparameters
+    hyperparameters["image_channels"] = fe.feed(fe.get_image_from_file(sample_image).unsqueeze(0)).shape[1]
+    hyperparameters["word_count"] = tk.counter
+    hyperparameters["encode_map"] = tk.encode_map
+
+    # Preparing folders for logging
+    for path in [checkpoints_folder, summary_folder]:
+        if not os.path.exists(path):
+            os.makedirs(path)
 
     # mock_image = torch.ones((32, 576, 7, 7)).to(torch.float32).to("cuda")
     # mock_caption = torch.ones((32, 20)).to(torch.int32).to("cuda")
@@ -88,9 +100,9 @@ def main():
     # dc.eval()
     # dc(mock_image, mock_caption)
 
-    # wr = SummaryWriter(summary_folder)
-    # trainer = Trainer(tk, fe, sh, checkpoints_folder, sample_image, wr, hyperparameters)
-    # trainer.train()
+    wr = SummaryWriter(summary_folder)
+    trainer = Trainer(tk, fe, sh, checkpoints_folder, sample_image, wr, hyperparameters)
+    trainer.train()
 
 
 if __name__ == "__main__":

model/multihead_attention.py
@@ -112,8 +112,7 @@ class MultiHeadAttention(nn.Module):
         affinity /= self.tau
 
         # Masked softmax
-        if mask:
-            # causal_mask = torch.tril(torch.ones(T_q, T_k, device=self.device)).view(1, 1, T_q, T_k)
+        if mask is not None:
             affinity += (mask == 0) * -1e9
 
         affinity = self.softmax(affinity)

model/transformer.py
@@ -15,9 +15,9 @@ class TransformerBlock(nn.Module):
         embeddings = kwargs["embeddings_number"]
         channels = kwargs["image_channels"]
         dropout_rate = kwargs["dropout_rate"]
-        context_length = kwargs["context_length"]
         heads_number = kwargs["heads_number"]
         device = kwargs["device"]
+        self.device = device
 
         # Self attention
         self.self_attention = MultiHeadAttention(
@@ -46,13 +46,15 @@ class TransformerBlock(nn.Module):
             nn.Linear(2 * embeddings, embeddings, device=device),
             nn.Dropout(dropout_rate),
         )
-        self.add_and_norm_3 = ResidualLayerNormalization(embeddings. device)
+        self.add_and_norm_3 = ResidualLayerNormalization(embeddings, device)
 
     def forward(self, image, caption):
-        sa = self.self_attention(caption, caption, caption, True)
+        T_q, T_k = caption.shape[1], caption.shape[1]
+        causal_mask = torch.tril(torch.ones(T_q, T_k, device=self.device)).view(1, 1, T_q, T_k)
+        sa = self.self_attention(caption, caption, caption, causal_mask)
         x = self.add_and_norm_1(sa, caption)
 
-        cr = self.cross_attention(x, image, image, False)
+        cr = self.cross_attention(x, image, image)
         x = self.add_and_norm_2(cr, x)
 
         ff = self.feed_forward(x)