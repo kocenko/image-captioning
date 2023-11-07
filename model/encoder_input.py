9fc1526ec92b96c58f8d386cef41272f16016466
Gabriel Kocenko <gabryelkocenko@gmail.com>
2023-11-07 09:51:34 +0100

    Moved positional embedding sequence to initializer

--- model/encoder_input.py
+++ model/encoder_input.py
@@ -17,13 +17,13 @@ class EncoderInput(nn.Module):
         features_num = 4 * 3 * image_size[0] * image_size[1]  # shifts * channels * height * width
         patches_num = (image_size[0] // patch_size) * (image_size[1] // patch_size)
 
-        self.device = device
         self.patch_tokenizer = ShiftedPatchTokenizer(image_size, shift_pixels, patch_size)
         self.positional_embedding = nn.Embedding(patches_num, embeddings, device=device)
         self.patch_embedding = nn.Embedding(features_num, embeddings, device=device)  # No padding performed
+        self.register_buffer('sequence_indices', torch.arange(patches_num, device=device))
 
     def forward(self, image: torch.Tensor) -> torch.Tensor:
         patches = self.patch_tokenizer(image)
         patch_embedding = self.patch_embedding(patches)
-        positional_embedding = self.positional_embedding(torch.arange(patches.shape[1], device=self.device))
+        positional_embedding = self.positional_embedding(self.sequence_indices)
         return patch_embedding + positional_embedding
