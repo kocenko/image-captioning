c74061cc6142aad25b6b88bc5ff46baac1c06a6a
Gabriel Kocenko <gabryelkocenko@gmail.com>
2023-11-07 09:08:26 +0100

    Defined encoder block

---
+++ model/encoder_block.py
@@ -0,0 +1,48 @@
+import torch
+from torch import nn
+
+from model.multihead_attention import MultiHeadAttention
+from model.add_and_norm import ResidualLayerNormalization
+
+
+class EncoderBlock(nn.Module):
+    def __init__(
+        self,
+        embeddings: int,
+        dropout_rate: float,
+        heads_num: int,
+        patches_num: int,
+        device: str,
+    ):
+        super().__init__()
+        self.self_attention = MultiHeadAttention(
+            input_shapes=(embeddings, embeddings, embeddings),
+            embeddings_number=embeddings,
+            heads_number=heads_num,
+            dropout_rate=dropout_rate,
+            device=device,
+            trainable_scale=True,
+        )
+        self.add_and_norm_1 = ResidualLayerNormalization(embeddings_number=embeddings, device=device)
+        self.feed_forward = nn.Sequential(
+            nn.Linear(embeddings, 4 * embeddings, device=device),
+            nn.GELU(),
+            nn.Linear(4 * embeddings, embeddings, device=device),
+            nn.Dropout(dropout_rate),
+        )
+        self.add_and_norm_2 = ResidualLayerNormalization(embeddings_number=embeddings, device=device)
+
+        # Used to ensure Locality Self Attention
+        self.register_buffer(
+            "diagonal_mask",
+            torch.sub(torch.ones(patches_num, patches_num), torch.eye(patches_num)).view(
+                1, 1, patches_num, patches_num
+            ),
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        sa = self.self_attention(x, x, x, self.diagonal_mask)
+        x = self.add_and_norm_1(sa, x)
+        ff = self.feed_forward(x)
+        x = self.add_and_norm_2(ff, x)
+        return x
